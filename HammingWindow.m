4767b0419b6762fc3efeaabdb201f1009e1aad9e
Yuri Loia <37004692+yuriloia@users.noreply.github.com>
2018-11-21 15:52:31 -0300

    Description added

--- HammingWindow.m
+++ HammingWindow.m
@@ -1,6 +1,5 @@
 function [ HammingW ] = HammingWindow( w,gamma )
-%UNTITLED2 Summary of this function goes here
-%   Detailed explanation goes here
+%Needs The DY function
 
     HammingW =(1/(2*pi))*( 1/2*DY(w,gamma) + 1/4*DY(w-pi/gamma,gamma) + 1/4*DY(w+pi/gamma,gamma));
 end
